c1fb4dece3ddf91a8e848ac44a87c0cfb14b3e11
kamal333k <kumawatkamal404@gmail.com>
2021-04-12 14:22:27 -0400

    best combination done

--- dp/targetsumFromArray.py
+++ dp/targetsumFromArray.py
@@ -34,6 +34,33 @@ class solution:
         return mem[target]
 
 
+
+    def helper(self, target, arr, mem):
+        if target in mem:
+            return mem[target]
+        if target == 0:
+            return []
+        
+        if target < 0:
+            return None
+        
+        comb = None
+        for i in arr:
+            val = self.helper(target - i, arr, mem)
+            if val != None:
+                val.append(i)
+                if comb == None or len(comb) > len(val):
+                    comb = val
+                # mem[target] = mem[target] + mem[target-i]
+        
+        mem[target] = comb
+        return mem[target]
+    def bestComination(self, target, arr):
+        mem = {}
+        self.helper(target, arr, mem)
+        return mem[target]
+
 c = solution()
-print(c.targetSum(301, [7,14]))
-print(c.targetSumThenReturnArray(8, [2,3,5]))
+# print(c.targetSum(301, [7,14]))
+# print(c.targetSumThenReturnArray(8, [2,3,5]))
+print(c.bestComination(8, [2,3,5]))
