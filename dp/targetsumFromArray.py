92ea481cc25b9a65b5c8bd46260944499c6f1240
kamal333k <kumawatkamal404@gmail.com>
2021-04-12 15:22:48 -0400

    all combinations doe

--- dp/targetsumFromArray.py
+++ dp/targetsumFromArray.py
@@ -2,7 +2,7 @@
 class solution:
     def targetSum(self, target, arr, mem={}):
         if target in mem:
-            return mem[target]
+            return None
         if target == 0:
             return True
         
@@ -37,7 +37,7 @@ class solution:
 
     def helper(self, target, arr, mem):
         if target in mem:
-            return mem[target]
+            return None
         if target == 0:
             return []
         
@@ -60,7 +60,36 @@ class solution:
         self.helper(target, arr, mem)
         return mem[target]
 
+    def allCombHelper(self, target, arr, mem):
+        if target in mem:
+            return None
+        if target == 0:
+            return [[]]
+        
+        if target < 0:
+            return None
+        
+        comb = None
+        for i in arr:
+            val = self.allCombHelper(target - i, arr, mem)
+            if val != None:
+                for e in val:
+                    e.append(i)
+                if comb == None:
+                    comb = list(val)
+                else:
+                    comb += list(val)
+                # mem[target] = mem[target] + mem[target-i]
+            val = None
+        mem[target] = comb
+        return mem[target]
+    def allCombination(self, target, arr):
+        mem = {}
+        self.allCombHelper(target, arr, mem)
+        return mem[target]
+
 c = solution()
-# print(c.targetSum(301, [7,14]))
-# print(c.targetSumThenReturnArray(8, [2,3,5]))
+print(c.targetSum(25, [7,14]))
+print(c.targetSumThenReturnArray(8, [2,3,5]))
 print(c.bestComination(8, [2,3,5]))
+print(c.allCombination(8, [2,3,5]))
